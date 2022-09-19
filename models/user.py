577529010679d691a49641b247d7e07c723280e8
DanielaIH <4427@holbertonschool.com>
2022-09-18 21:29:34 -0500

    UPDATE models/user.py models/place.py models/city.py

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,6 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all, delete", backref="user")
     else:
         email = ''
         password = ''
