f500fe9bb40af358aee0ed9d80610be64b21ec17
DanielaIH <4427@holbertonschool.com>
2022-09-18 23:56:10 -0500

    UPDATE db format final

--- models/base_model.py
+++ models/base_model.py
@@ -27,13 +27,30 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.
+                strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = kwargs['updated_at']
+            else:
+                self.updated_at = datetime.now()
+                kwargs['updated_at'] = self.updated_at
+
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.
+                strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.created_at = kwargs['created_at']
+            else:
+                self.created_at = datetime.now()
+                kwargs['created_at'] = self.created_at
+
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
 
             self.__dict__.update(kwargs)
+
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
