23efdbff21fa801c410bd8624a0b8498401bf049
DanielaIH <4427@holbertonschool.com>
2022-09-18 21:01:54 -0500

    ADD users class in database _

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_t == "db":
