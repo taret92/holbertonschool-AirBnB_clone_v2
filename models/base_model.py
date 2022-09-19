aed0f72c44f0958145dec1de77f9172aba80b368
DanielaIH <4427@holbertonschool.com>
2022-09-18 23:12:28 -0500

    UPDATE Many-to-Many relationship

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
--- models/place.py
+++ models/place.py
@@ -6,6 +6,15 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
 if models.storage_t == "db":
+    class place_amenity(BaseModel, Base):
+        __tablename__ = 'place_amenity'
+        place_id = Column(String(60), ForeignKey('places.id',
+                          onupdate='CASCADE', ondelete='CASCADE'),
+                          primary_key=True)
+        amenity_id = Column(String(60), ForeignKey('amenities.id',
+                            onupdate='CASCADE', ondelete='CASCADE'),
+                            primary_key=True)
+
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
@@ -75,3 +84,9 @@ class Place(BaseModel, Base):
                 if amenity.place_id == self.id:
                     amenity_list.append(amenity)
             return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """setter amenities"""
+            if (type(obj) == Amenity):
+                self.amenity_ids.append(obj.id)
